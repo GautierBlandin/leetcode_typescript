d3bf4c262668c39cf5bfb6727ff551cbbfcc0b6a
Gautier <gautier.blandin@gmail.com>
2024-04-10 23:44:49 +0200

    feat(LE-44): add working wildcard matching code

---
+++ src/hard/wildcardMatching.ts
@@ -0,0 +1,51 @@
+export function isMatch(s: string, p: string): boolean {
+  const matchMap = new Map<string, boolean>();
+
+  return isMatchRecursive(s, p, 0, 0, matchMap);
+}
+
+function isMatchRecursive(s: string, p: string, i: number, j: number, matchMap: Map<string, boolean>): boolean {
+  if (matchMap.get(`${i},${j}`) !== undefined) {
+    return matchMap.get(`${i},${j}`) as boolean;
+  }
+
+  if (i === s.length && j === p.length) {
+    return memoize(true, i, j, matchMap);
+  }
+
+  if (j === p.length) return memoize(false, i, j, matchMap);
+
+  if (i === s.length) {
+    if (p[j] !== '*') return memoize(false, i, j, matchMap);
+    return memoize(isMatchRecursive(s, p, i, j + 1, matchMap), i, j, matchMap);
+  }
+
+  if (!matchSingleCharacter(s[i], p[j])) return memoize(false, i, j, matchMap);
+
+  if (p[j] === '*') {
+    return memoize(
+      isMatchRecursive(s, p, i + 1, j + 1, matchMap)
+      || isMatchRecursive(s, p, i + 1, j, matchMap)
+      || isMatchRecursive(s, p, i, j + 1, matchMap),
+      i,
+      j,
+      matchMap,
+    );
+  }
+
+  return memoize(
+    isMatchRecursive(s, p, i + 1, j + 1, matchMap),
+    i,
+    j,
+    matchMap,
+  );
+}
+
+function memoize(result: boolean, i: number, j: number, matchMap: Map<string, boolean>) {
+  matchMap.set(`${i},${j}`, result);
+  return result;
+}
+
+function matchSingleCharacter(c: string, pc: string): boolean {
+  return c === pc || pc === '?' || pc === '*';
+}
