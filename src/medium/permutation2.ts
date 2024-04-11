22ce7b90ed8442cd432d2d713cf6001e9e09b9de
Gautier <gautier.blandin@gmail.com>
2024-04-11 13:51:50 +0200

    feat(LC-47): improve permutation2 performance by using a set

--- src/medium/permutation2.ts
+++ src/medium/permutation2.ts
@@ -4,35 +4,19 @@ export function permuteUnique(nums: number[]): number[][] {
   for (let i = 1; i < nums.length; i++) {
     // compute the new permutations with duplicates
     const newPermutations: number[][] = [];
+    const newPermutationSet: Set<string> = new Set();
     for (let j = 0; j <= i; j++) {
       for (let k = 0; k < permutations.length; k++) {
-        newPermutations.push([...permutations[k].slice(0, j), nums[i], ...permutations[k].slice(j)]);
-      }
-    }
-
-    // remove duplicates
-    for (let j = 0; j < newPermutations.length; j++) {
-      const toMatch = newPermutations[j];
-      let k = j + 1;
-      while (k < newPermutations.length) {
-        if (subPermAreEqual(toMatch, newPermutations[k])) {
-          newPermutations.splice(k, 1);
-        } else {
-          k++;
+        const newPermutation = [...permutations[k].slice(0, j), nums[i], ...permutations[k].slice(j)];
+        const hash = newPermutation.join(',');
+        if (!newPermutationSet.has(hash)) {
+          newPermutations.push(newPermutation);
+          newPermutationSet.add(hash);
         }
       }
     }
-
     permutations = newPermutations;
   }
 
   return permutations;
-}
-
-function subPermAreEqual(a: number[], b: number[]) {
-  // we assume equal length
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) return false;
-  }
-  return true;
 }
