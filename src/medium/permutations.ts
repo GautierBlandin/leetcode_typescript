853c12bdd786b4e15b7c84a8604d1d501a764425
Gautier <gautier.blandin@gmail.com>
2024-04-11 11:08:46 +0200

    feat(LC-46): add permutation iterative solution

--- src/medium/permutations.ts
+++ src/medium/permutations.ts
@@ -1,11 +1,11 @@
-export function permute(nums: number[]): number[][] {
+export function permuteRecursive(nums: number[]): number[][] {
   if (nums.length === 1) {
     return [[nums[0]]];
   }
 
   const n = nums.length;
   const lastNum = nums.pop() as number;
-  const subPermutations = permute(nums);
+  const subPermutations = permuteRecursive(nums);
   const newPermutations: number[][] = [];
 
   for (let i = 0; i < n; i++) {
@@ -15,4 +15,20 @@ export function permute(nums: number[]): number[][] {
   }
 
   return newPermutations;
+}
+
+export function permute(nums: number[]): number[][] {
+  let permutations: number[][] = [[nums[0]]];
+
+  for (let i = 1; i < nums.length; i++) {
+    const newPermutations: number[][] = [];
+    for (let j = 0; j <= i; j++) {
+      for (let k = 0; k < permutations.length; k++) {
+        newPermutations.push([...permutations[k].slice(0, j), nums[i], ...permutations[k].slice(j)]);
+      }
+    }
+    permutations = newPermutations;
+  }
+
+  return permutations;
 }
