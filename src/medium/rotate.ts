d0f8b483763cb917ae1f18dbde6d9653d02d6ffa
Gautier <gautier.blandin@gmail.com>
2024-04-17 14:54:14 +0200

    feat(LC-48): improve code by not using centered coordinates

--- src/medium/rotate.ts
+++ src/medium/rotate.ts
@@ -4,34 +4,11 @@ export function rotate(matrix: number[][]): void {
 
   for (let i = 0; i < Math.floor(n / 2); i++) {
     for (let j = 0; j < Math.ceil(n / 2); j++) {
-      const [centeredI, centeredJ] = toCentered(i, j, n);
-      const [val1i, val1j] = fromCentered(centeredJ, -centeredI, n);
-      const [val2i, val2j] = fromCentered(-centeredI, -centeredJ, n);
-      const [val3i, val3j] = fromCentered(-centeredJ, centeredI, n);
-      const val0 = matrix[i][j];
-      const val1 = matrix[val1i][val1j];
-      const val2 = matrix[val2i][val2j];
-      const val3 = matrix[val3i][val3j];
-      matrix[i][j] = val3;
-      matrix[val1i][val1j] = val0;
-      matrix[val2i][val2j] = val1;
-      matrix[val3i][val3j] = val2;
+      const temp = matrix[i][j];
+      matrix[i][j] = matrix[n - 1 - j][i];
+      matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+      matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+      matrix[j][n - 1 - i] = temp;
     }
   }
-}
-
-function toCentered(i: number, j: number, n: number): [number, number] {
-  return [i - Math.floor(n / 2), j - Math.floor(n / 2)];
-}
-
-function fromCentered(i: number, j: number, n: number): [number, number] {
-  if (n % 2 === 1) return [i + Math.floor(n / 2), j + Math.floor(n / 2)];
-  return [singleFromCenteredEvenN(i, n), singleFromCenteredEvenN(j, n)];
-}
-
-function singleFromCenteredEvenN(k: number, n: number) {
-  if (k < 0) {
-    return k + n / 2;
-  }
-  return k + n / 2 - 1;
 }
