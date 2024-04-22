1563accbb4b10e69409936a07d6c97485f96645a
Gautier <gautier.blandin@gmail.com>
2024-04-22 23:37:53 +0200

    feat(LC-52): add total queens solution

---
+++ src/hard/nQueensII.ts
@@ -0,0 +1,91 @@
+export function totalNQueens(n: number): number {
+  const numberOfQueens: number = 0;
+
+  const context = {
+    numberOfQueens,
+    result: 0,
+    currentBoard: new Array(n).fill(undefined).map(() => '.'.repeat(n)),
+  };
+
+  nQueensRecursive(0, context);
+
+  return context.result;
+}
+
+type Context = {
+  numberOfQueens: number;
+  result: number;
+  currentBoard: string[];
+};
+
+/**
+ *
+ * @param minIndex min index for placing the next queen in flattened coordinate
+ * @param context recursive context to pass along
+ */
+function nQueensRecursive(minIndex: number, context: Context) {
+  const n = context.currentBoard.length;
+  for (let i = minIndex; i < n * n; i += 1) {
+    const row = Math.floor(i / n);
+    const col = i % n;
+
+    if (isNotAttacked(row, col, context.currentBoard)) {
+      placeQueen(row, col, context.currentBoard);
+      context.numberOfQueens += 1;
+
+      if (context.numberOfQueens === n) {
+        context.result += 1;
+      } else {
+        nQueensRecursive(i + 1, context);
+      }
+
+      unplaceQueen(row, col, context.currentBoard);
+      context.numberOfQueens -= 1;
+    }
+  }
+}
+
+function isNotAttacked(row: number, col: number, currentBoard: string[]) {
+  const n = currentBoard.length;
+  for (let i = 0; i < n; i += 1) {
+    if (i === row) continue;
+    if (currentBoard[i][col] === 'Q') return false;
+  }
+  for (let i = 0; i < n; i += 1) {
+    if (i === col) continue;
+    if (currentBoard[row][i] === 'Q') return false;
+  }
+  for (let i = 0; i < n; i += 1) {
+    const dRow = row - i;
+    const dCol = col - i;
+    if (dRow < 0 || dCol < 0) break;
+    if (currentBoard[dRow][dCol] === 'Q') return false;
+  }
+  for (let i = 0; i < n; i += 1) {
+    const dRow = row + i;
+    const dCol = col + i;
+    if (dRow >= n || dCol >= n) break;
+    if (currentBoard[dRow][dCol] === 'Q') return false;
+  }
+  for (let i = 0; i < n; i += 1) {
+    const dRow = row - i;
+    const dCol = col + i;
+    if (dRow < 0 || dCol >= n) break;
+    if (currentBoard[dRow][dCol] === 'Q') return false;
+  }
+  for (let i = 0; i < n; i += 1) {
+    const dRow = row + i;
+    const dCol = col - i;
+    if (dRow >= n || dCol < 0) break;
+    if (currentBoard[dRow][dCol] === 'Q') return false;
+  }
+  return true;
+}
+
+function placeQueen(row: number, col: number, board: string[]) {
+  board[row] = `${board[row].substring(0, col)}Q${board[row].substring(col + 1)}`;
+}
+
+function unplaceQueen(row: number, col: number, board: string[]) {
+  board[row] = `${board[row].substring(0, col)}.${board[row].substring(col + 1)}`;
+}
