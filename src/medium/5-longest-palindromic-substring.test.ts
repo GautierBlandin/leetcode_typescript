4cec28b52ef612c9f6dac3ef44a7646c4210d333
Gautier <gautier.blandin@gmail.com>
2024-11-06 09:52:24 +0100

    feat: add expand around center approach for longest palindrome

--- src/medium/5-longest-palindromic-substring.test.ts
+++ src/medium/5-longest-palindromic-substring.test.ts
@@ -1,4 +1,47 @@
+// Expand Around Center approach. Validated against the test suite & LC tests
 function longestPalindrome(s: string): string {
+  // Expand around center approach
+  let maxPalindrome = '';
+
+  // single-center palindrome
+  for (let startPosition = 0; startPosition < s.length; startPosition += 1) {
+    let expandInterval = 0;
+
+    while (
+      startPosition - expandInterval - 1 >= 0
+    && startPosition + expandInterval + 1 < s.length
+    && s[startPosition - expandInterval - 1] === s[startPosition + expandInterval + 1]
+    ) {
+      expandInterval += 1;
+    }
+
+    if (maxPalindrome.length < (1 + 2 * (expandInterval))) {
+      maxPalindrome = s.slice(startPosition - expandInterval, startPosition + expandInterval + 1);
+    }
+  }
+
+  // bi-center palindrome
+  for (let startPosition = 0; startPosition < s.length; startPosition += 1) {
+    let expandInterval = 0;
+
+    while (
+      startPosition - expandInterval - 1 >= 0
+      && startPosition + expandInterval < s.length
+      && s[startPosition - expandInterval - 1] === s[startPosition + expandInterval]
+    ) {
+      expandInterval += 1;
+    }
+
+    if (maxPalindrome.length < (2 * (expandInterval))) {
+      maxPalindrome = s.slice(startPosition - expandInterval, startPosition + expandInterval);
+    }
+  }
+
+  return maxPalindrome;
+}
+
+// Brute-force approach. Validated against the test suite & LC test
+function longestPalindromeBruteForce(s: string): string {
   let maxPalindrome = '';
 
   for (let i = 0; i < s.length; i += 1) {
