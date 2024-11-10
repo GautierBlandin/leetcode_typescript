61174e1280dd3997576f1916b1f29210d6a0640f
Gautier <gautier.blandin@gmail.com>
2024-11-10 16:44:33 +0100

    feat: update tests and implementation of the regex matcher to meet new requirements

--- src/hard/10-regular-expression-matching.test.ts
+++ src/hard/10-regular-expression-matching.test.ts
@@ -3,18 +3,29 @@ export function isMatch(s: string, p: string): boolean {
    * dpTable[i][j] is true if s.slice(i) matches p.slice(j)
    * The overall match is true if at the end of the algorithm, dpTable[s.length][p.length] is true
    */
-  const dpTable: boolean[][] = new Array(s.length + 1).fill(0).map(() => new Array(p.length + 1).fill(false));
+  const dpTable = Array.from({ length: s.length + 1 }).map(() => Array.from({ length: p.length + 1 }, () => false));
 
   for (let i = 0; i < s.length + 1; i += 1) {
     for (let j = 0; j < p.length + 1; j += 1) {
+      // s[i - 1] is the character we are working with in the string
+      // p[j - 1] is the character we are working with in the pattern
+      if (i < 0 || j < 0) {
+        continue;
+      }
       if (i === 0 && j === 0) {
+        // Empty pattern matches with empty string
         dpTable[i][j] = true;
       } else if (j === 0) {
+        // j === 0 means that we slice the pattern at 0, aka the pattern is empty. An empty pattern doesn't match with anything non-empty.
         dpTable[i][j] = false;
       } else if (i === 0) {
-        dpTable[i][j] = dpTable[i][j - 1] && p[j - 1] === '*';
+        // i === 0 means that we slice the string at 0, aka the string is empty. A match is only possible with star patterns.
+        dpTable[i][j] = p[j - 1] === '*' && dpTable[i][j - 2];
       } else if (p[j - 1] === '*') {
-        dpTable[i][j] = dpTable[i - 1][j] || dpTable[i][j - 1];
+        // In case the pattern character is a star, a match is present if:
+        // - The string character matches with the character preceding the star
+        // - OR The string matches with the pattern with the star combination (star + preceding character) removed
+        dpTable[i][j] = (singleCharMatcher(s[i - 1], p[j - 2]) && dpTable[i - 1][j]) || dpTable[i][j - 2];
       } else {
         dpTable[i][j] = singleCharMatcher(s[i - 1], p[j - 1]) && dpTable[i - 1][j - 1];
       }
@@ -24,22 +35,8 @@ export function isMatch(s: string, p: string): boolean {
   return dpTable[s.length][p.length];
 }
 
-export function isMatchBruteForce(s: string, p: string): boolean {
-  if (s.length === 0) {
-    return p.length === 0 || (p[0] === '*' && isMatchBruteForce(s, p.slice(1)));
-  }
-
-  if (p.length === 0) return false;
-
-  if (p[0] === '*') {
-    return isMatchBruteForce(s.slice(1), p) || isMatchBruteForce(s, p.slice(1));
-  }
-
-  return (singleCharMatcher(s[0], p[0]) && isMatchBruteForce(s.slice(1), p.slice(1)));
-}
-
-function singleCharMatcher(c: string, pc: string): boolean {
-  return (c === pc || pc === '.' || pc === '*');
+function singleCharMatcher(c: string, pc: string | undefined): boolean {
+  return (c === pc || pc === '.');
 }
 
 describe('isMatch', () => {
@@ -59,32 +56,24 @@ describe('isMatch', () => {
     expect(isMatch(arg, '.')).toBe(true);
   });
 
-  it.each(['', 'a, aa, ab, ba, adb, adcf'])('%s matches with *', (arg) => {
-    expect(isMatch(arg, '*')).toBe(true);
-  });
-
-  it.each(['a', 'aa, ab, abc, aaa, adcb'])('%s matches with a*', (arg) => {
+  it.each(['', 'a', 'aa', 'aaa', 'aaaa', 'aaaaa'])('%s matches with a*', (arg) => {
     expect(isMatch(arg, 'a*')).toBe(true);
   });
 
-  it.each(['', 'b', 'ba', 'baaa', 'feafda'])('%s does not match with a*', (arg) => {
-    expect(isMatch(arg, 'a*')).toBe(false);
+  it.each(['', 'bc', 'bd', 'aaaa', 'feafda'])('%s does not match with ba*', (arg) => {
+    expect(isMatch(arg, 'ba*')).toBe(false);
   });
 
-  it.each(['a', 'aa', 'ba', 'aba', 'bcdfa', 'aaaaa'])('%s matches with *a', (arg) => {
-    expect(isMatch(arg, '*a')).toBe(true);
+  it('matches bbba with b*a', () => {
+    expect(isMatch('bbba', 'b*a')).toBe(true);
   });
 
-  it.each(['ab', 'aiuoiuob', 'asb', 'aab', 'abb'])('%s matches with a*b', (arg) => {
-    expect(isMatch(arg, 'a*b')).toBe(true);
-  });
-
-  it('handles multiple * in a row', () => {
-    expect(isMatch('abc', 'a**c')).toBe(true);
+  it.each(['a', 'ba', 'bba', 'bbba', 'bbbba', 'bbbbbba'])('%s matches with b*a', (arg) => {
+    expect(isMatch(arg, 'b*a')).toBe(true);
   });
 
   it('works on a somewhat complex expression', () => {
-    expect(isMatch('theworldisabeautifulplace', 't.e*isabeau.....pl*')).toBe(true);
+    expect(isMatch('theworldisabeautifulplace', 't.e.*isabeau.....pl.*')).toBe(true);
   });
 
   it('works on a somewhat complex expression', () => {
