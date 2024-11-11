b85c32f2451c26b073cb1b7075fee33907a6261a
Gautier <gautier.blandin@gmail.com>
2024-11-11 23:31:00 +0100

    refactor(LC-11): rename height variables for clarity

--- src/medium/11-container-with-most-water.ts
+++ src/medium/11-container-with-most-water.ts
@@ -15,3 +15,39 @@ function maxAreaBruteForce(heights: number[]): number {
 
   return maxFoundArea;
 }
+
+export function maxArea(heights: number[]): number {
+  // Approach:
+  // Start with a pointer on the left and a pointer on the right.
+
+  let leftPointer = 0;
+  let maximumLeftHeight = heights[leftPointer];
+  let rightPointer = heights.length - 1;
+  let maximumRightHeight = heights[rightPointer];
+  let maximumArea = Math.min(maximumLeftHeight, maximumRightHeight) * (rightPointer - leftPointer);
+
+  while (leftPointer < rightPointer) {
+    if (maximumLeftHeight <= maximumRightHeight) {
+      // increment left pointer
+      leftPointer += 1;
+      // check if the height is greater than the current maximum height
+      if (heights[leftPointer] > maximumLeftHeight) {
+        // if that is the case, we have a candidate for a new maximum area
+        // check if the new area is bigger, and update the height either way
+        maximumArea = Math.max(maximumArea, Math.min(heights[leftPointer], maximumRightHeight) * (rightPointer - leftPointer));
+        maximumLeftHeight = heights[leftPointer];
+      }
+    } else {
+      // maximumLeftHeight > maximumRightHeight here
+      // decrement right pointer
+      rightPointer -= 1;
+      // check if the right height is greater than the current maximum right height
+      if (heights[rightPointer] > maximumRightHeight) {
+        maximumArea = Math.max(maximumArea, Math.min(maximumLeftHeight, heights[rightPointer]) * (rightPointer - leftPointer));
+        maximumRightHeight = heights[rightPointer];
+      }
+    }
+  }
+
+  return maximumArea;
+}
