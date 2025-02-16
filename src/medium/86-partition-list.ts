c1a5ec0b9f8eb09bf266a72625d02a0227ea3db2
Gautier <gautier.blandin@gmail.com>
2025-02-16 13:05:13 +0100

    LC-86: simpler implementation using two linked lists

--- src/medium/86-partition-list.ts
+++ src/medium/86-partition-list.ts
@@ -1,54 +1,38 @@
 import { ListNode } from '../common/ListNode';
 
 export function partition(head: ListNode | null, x: number): ListNode | null {
+  // New implementation idea:
+  // Create a left linked list and a right linked list, merging them at the end
+
   if (!head) return null;
 
-  let newHead: ListNode | null = null;
-  if (head.val < x) newHead = head;
+  let leftPartitionStart: ListNode | null = null;
+  let leftPartitionEnd: ListNode | null = null;
+  let rightPartitionStart: ListNode | null = null;
+  let rightPartitionEnd: ListNode | null = null;
 
-  let prev: ListNode | null = null;
-  let cur: ListNode = head;
+  let cur: ListNode | null = head;
 
-  // First part of the algorithm: skip until we reach the first interesting value
-  while (cur.val < x) {
-    // Terminate early if the whole linked list has been skipped
-    if (!cur.next) {
-      return head;
+  while (cur) {
+    if (cur.val < x) {
+      if (!leftPartitionStart || !leftPartitionEnd) {
+        leftPartitionStart = cur;
+        leftPartitionEnd = cur;
+      } else {
+        leftPartitionEnd.next = cur;
+        leftPartitionEnd = cur;
+      }
+    } else if (!rightPartitionStart || !rightPartitionEnd) {
+      rightPartitionStart = cur;
+      rightPartitionEnd = cur;
+    } else {
+      rightPartitionEnd.next = cur;
+      rightPartitionEnd = cur;
     }
-
-    prev = cur;
     cur = cur.next;
   }
 
-  // At this point, cur.val is guaranteed to be x or greater and should become the left-most value
-  // of the right partition
-
-  const rightPartitionLeft: ListNode = cur;
-  let rightPartitionRight: ListNode = cur;
-
-  while (rightPartitionRight.next) {
-    const next = rightPartitionRight.next;
-    // Case 1: The next node is >= x and should be part of right partition
-    if (next.val >= x) {
-      rightPartitionRight = rightPartitionRight.next;
-    } else {
-      // Case 2: The next node is < x and should be moved in front of the right partition
-      // If we haven't determined a newHead yet (the first element to be < x), set it.
-      if (!newHead) {
-        newHead = next;
-      }
-
-      rightPartitionRight.next = next.next;
-      next.next = rightPartitionLeft;
-
-      if (prev) {
-        prev.next = next;
-      }
-      prev = next;
-    }
-  }
-
-  if (newHead) return newHead;
-  // There were no nodes smaller than x, the linked list is left intact and we directly return head.
-  return head;
+  if (leftPartitionEnd) leftPartitionEnd.next = rightPartitionStart;
+  if (rightPartitionEnd) rightPartitionEnd.next = null;
+  return leftPartitionStart ?? rightPartitionStart;
 }
