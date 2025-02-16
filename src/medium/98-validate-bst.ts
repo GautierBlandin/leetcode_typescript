d7149750ce44acf1b5cd487de53b51e4bd2f7576
Gautier <gautier.blandin@gmail.com>
2025-02-16 15:45:47 +0100

    LC-98: update validate bst to use a lower/higher bound approach

--- src/medium/98-validate-bst.ts
+++ src/medium/98-validate-bst.ts
@@ -1,20 +1,26 @@
 import { TreeNode } from '../common/TreeNode';
 
 function isValidBST(root: TreeNode | null): boolean {
-  const content: number[] = [];
+  let valid = true;
 
-  function process(node: TreeNode | null) {
+  function process(node: TreeNode | null, upperBound: number, lowerBound: number) {
     if (!node) return;
-    process(node.left);
-    content.push(node.val);
-    process(node.right);
+
+    if (node.val >= upperBound) {
+      valid = false;
+      return;
+    }
+
+    if (node.val <= lowerBound) {
+      valid = false;
+      return;
+    }
+
+    process(node.left, Math.min(upperBound, node.val), lowerBound);
+    process(node.right, upperBound, Math.max(lowerBound, node.val));
   }
 
-  process(root);
+  process(root, Infinity, -Infinity);
 
-  for (let i = 0; i < content.length - 1; i += 1) {
-    if (content[i] >= content[i + 1]) return false;
-  }
-
-  return true;
+  return valid;
 }
